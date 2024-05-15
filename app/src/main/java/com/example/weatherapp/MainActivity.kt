375eb7d99b5a7ca809457af7b9c9212b4ae24dfd
ArsenDev <arsen.nedogibchencko@gmail.com>
2024-05-15 23:26:23 +0300

    first version

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.weatherapp
+
+import android.app.DownloadManager.Request
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.privacysandbox.tools.core.model.Method
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.weatherapp.databinding.ActivityMainBinding
+import com.example.weatherapp.fragment.MainFragment
+import org.json.JSONObject
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager
+            .beginTransaction().replace(R.id.placeHolder, MainFragment.newInstance()).commit()
+        }
+    }
